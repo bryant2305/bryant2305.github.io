a4c417fa24169bdf4f89e38e51947d6590b29331
Bryant Perez Garcia <119883367+bryant2305@users.noreply.github.com>
2025-02-13 11:26:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.mjs';
 import { VitePWA } from 'vite-plugin-pwa';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          metaTitle: CONFIG.seo.title,
-          metaDescription: CONFIG.seo.description,
-          metaImageURL: CONFIG.seo.imageURL,
-        },
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        navigateFallback: undefined,
+      },
+      includeAssets: ['logo.png'],
+      manifest: {
+        name: 'Portfolio',
+        short_name: 'Portfolio',
+        description: 'Personal Portfolio',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
-    ...(CONFIG.enablePWA
-      ? [
-          VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-              navigateFallback: undefined,
-            },
-            includeAssets: ['logo.png'],
-            manifest: {
-              name: 'Portfolio',
-              short_name: 'Portfolio',
-              description: 'Personal Portfolio',
-              icons: [
-                {
-                  src: 'logo.png',
-                  sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
-                  type: 'image/png',
-                },
-              ],
-            },
-          }),
-        ]
-      : []),
   ],
-  define: {
-    CONFIG: CONFIG,
+  css: {
+    postcss: {
+      plugins: [tailwind(tailwindConfig), autoprefixer],
+    },
   },
 });
