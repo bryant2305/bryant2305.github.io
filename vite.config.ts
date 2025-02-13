cf025386a8532dc350c6a0f57b6b60ea2724517f
Bryant Perez Garcia <119883367+bryant2305@users.noreply.github.com>
2025-02-13 11:31:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,50 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import tailwindConfig from './tailwind.config.mjs';
 import { VitePWA } from 'vite-plugin-pwa';
+import CONFIG from './gitprofile.config';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+// Asegurar que `seo` no sea undefined
+const seoConfig = CONFIG.seo || { title: '', description: '', imageURL: '' };
 
 export default defineConfig({
-  base: '/',
+  base: CONFIG.base || '/',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        navigateFallback: undefined,
-      },
-      includeAssets: ['logo.png'],
-      manifest: {
-        name: 'Portfolio',
-        short_name: 'Portfolio',
-        description: 'Personal Portfolio',
-        icons: [
-          {
-            src: 'logo.png',
-            sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
-            type: 'image/png',
-          },
-        ],
+    createHtmlPlugin({
+      inject: {
+        data: {
+          metaTitle: seoConfig.title,
+          metaDescription: seoConfig.description,
+          metaImageURL: seoConfig.imageURL,
+        },
       },
     }),
+    ...(CONFIG.enablePWA
+      ? [
+          VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+              navigateFallback: undefined,
+            },
+            includeAssets: ['logo.png'],
+            manifest: {
+              name: 'Portfolio',
+              short_name: 'Portfolio',
+              description: 'Personal Portfolio',
+              icons: [
+                {
+                  src: 'logo.png',
+                  sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
+                  type: 'image/png',
+                },
+              ],
+            },
+          }),
+        ]
+      : []),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwind(tailwindConfig), autoprefixer],
-    },
+  define: {
+    CONFIG: CONFIG,
   },
 });
